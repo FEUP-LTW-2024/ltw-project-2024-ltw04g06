7557b5adb0c35f1a69e7b1546aa689721f15122f
Alex Costa <up202207499@fe.up.pt>
2024-04-21 18:17:06 +0100

    main page done

--- database/populate.sql
+++ database/populate.sql
@@ -13,9 +13,9 @@ VALUES
 -- Insert sample data into the Item table
 INSERT OR IGNORE INTO Item (itemID, sellerID, categoryID, brand, model, sizeID, conditionID, description, price, images, statusID)
 VALUES 
-    (1, 1, 1, 'Brand A', 'Model X', 1, 1, 'Description of item 1', 100.00, 'image_url_1,image_url_2,image_url_3', 1),
-    (2, 1, 2, 'Brand B', 'Model Y', 2, 2, 'Description of item 2', 200.00, 'image_url_4,image_url_5,image_url_6', 1),
-    (3, 2, 2, 'Brand C', 'Model Z', 3, 3, 'Description of item 3', 300.00, 'image_url_4,image_url_5,image_url_6', 2);
+    (1, 1, 1, 'Brand A', 'Model X', 1, 1, 'Description of item 1', 100.00, 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fmixtronica.com%2F24v-incandescente%2F19655-lampada-incandescente-e27-24v-60w-970lm-philips-8711500090188.html&psig=AOvVaw2cgsMuEGAr_clW7KM93Bf1&ust=1713794561765000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCJj95rG804UDFQAAAAAdAAAAABAE', 1),
+    (2, 1, 2, 'Brand B', 'Model Y', 2, 2, 'Description of item 2', 200.00, 'https://www.google.com/imgres?q=lampada&imgurl=https%3A%2F%2Fwww.assets.signify.com%2Fis%2Fimage%2FSignify%2FLEDBulb_E27_A60_5_5W-SPP%3Fwid%3D494%26hei%3D435%26%24pnglarge%24&imgrefurl=https%3A%2F%2Fwww.lighting.philips.pt%2Fconsumer%2Fp%2Fled-lampada-de-filamento-ambar-25-w-a60-e27%2F8719514315433&docid=GqeC-6invrCPSM&tbnid=xR6sQDgPmaXJ9M&vet=12ahUKEwi426qxvNOFAxU52QIHHU6NCtAQM3oECGQQAA..i&w=494&h=435&hcb=2&ved=2ahUKEwi426qxvNOFAxU52QIHHU6NCtAQM3oECGQQAA', 1),
+    (3, 2, 2, 'Brand C', 'Model Z', 3, 3, 'Description of item 3', 300.00, 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.tecniled.pt%2Floja%2Flampadas-led%2Flampadas-led-e27%2Flampada-led-7w-2-7k-e27%2F&psig=AOvVaw2cgsMuEGAr_clW7KM93Bf1&ust=1713794561765000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCJj95rG804UDFQAAAAAdAAAAABAQ', 2);
 
 -- Insert sample data into the ShippingForm table
 INSERT OR IGNORE INTO ShippingForm (shippingFormID, itemID, sellerID, buyerID, address, date)
--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const showFormButton = document.getElementById('showFormButton');
 const elementToToggle = document.getElementById('elementToToggle');
 
-
     showFormButton.addEventListener('click', () => {
         if (elementToToggle.style.display == 'none') elementToToggle.style.display = 'block';
         else elementToToggle.style.display = 'none';
