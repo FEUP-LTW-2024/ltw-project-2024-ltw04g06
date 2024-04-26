cb6a2ce95a1c03820e0bd4cdb4b04f731a1957ad
dimochii <diana.mm.nunes.28@gmail.com>
2024-04-26 11:39:22 +0100

    small actions tweak

--- database/connectdb.php
+++ database/connectdb.php
@@ -1,7 +1,7 @@
 <?php
 
 function getDatabaseConnection() : PDO {
-    $db = new PDO('sqlite:' . __DIR__ . '/../database/database.db');
+    $db = new PDO('sqlite:' . __DIR__ . '/../database/create.db');
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
