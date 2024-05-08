d6947ab8f3ef0d07888afaa515bfe20d8afc45c8
dimochii <diana.mm.nunes.28@gmail.com>
2024-05-08 14:40:16 +0100

    add getAllUsers

--- classes/user.class.php
+++ classes/user.class.php
@@ -64,6 +64,16 @@ require_once(__DIR__ . '/shoppingCart.class.php');
       );
     }
 
+    static function getAllUsers(PDO $db) {
+      $preparedStmt = $db->prepare( 'SELECT * FROM User');
+      $preparedStmt->execute();
+     $users = array();
+      while($user = $preparedStmt->fetch()){
+        $users[] = self::getUser($db, $user ['userID']);
+      }
+    return $users;
+    }
+
 
 
     static function getUserWishlist(PDO $db, int $userID) {
