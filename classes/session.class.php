83412e3b6a7f2c2af93952f0903329a6e67c868b
dimochii <diana.mm.nunes.28@gmail.com>
2024-05-09 21:47:10 +0100

    session messages

--- classes/session.class.php
+++ classes/session.class.php
@@ -1,4 +1,5 @@
 <?php
+require_once(__DIR__ . '/../database/validate_input.php');
 
 class Session {
     private array $msgs;
--- pages/signIn.php
+++ pages/signIn.php
@@ -1,6 +1,8 @@
 <?php 
     require_once(__DIR__ . '/../templates/logs.php');
+    require_once(__DIR__ . '/../classes/session.class.php'); // Add this line
 
+    $session = new Session(); 
     displayNameLogo();
-    signInBox();
+    signInBox($session);
 ?>
