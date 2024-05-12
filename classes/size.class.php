d8245fc6956d3259a89656f5a085fd7c8ad66598
dimochii <diana.mm.nunes.28@gmail.com>
2024-05-12 18:28:20 +0100

    add size get and add functions

--- classes/size.class.php
+++ classes/size.class.php
@@ -3,17 +3,34 @@
 declare(strict_types = 1);
 
 class Size {
-	public int $sizeID;
-	public string $name;
+    public int $sizeID;
+    public string $name;
 
+    public function __construct(int $sizeID, string $name) {
+        $this->sizeID = $sizeID;
+        $this->name = $name;
+    }
 
-	public function __construct(int $sizeID, string $name) {
-		$this->id = $sizeID;
-		$this->name = $name;
-	}
+    static function getSize(PDO $db, int $sizeID): ?Size {
+        $preparedStmt = $db->prepare('SELECT * FROM Size WHERE sizeID = ?');
+        $preparedStmt->execute([$sizeID]);
+        $sizeData = $preparedStmt->fetch();
 
+        if (!$sizeData) {
+            throw new Exception("Size not found with ID: $sizeID");
+            return null;
+        }
 
+        return new Size(
+            $sizeData['sizeID'],
+            $sizeData['name']
+        );
+    }
 
+    static function addSize(PDO $db, int $sizeID, string $name) {
+        $preparedStmt = $db->prepare("INSERT INTO Size (sizeID, name) VALUES(?, ?)");
+        $preparedStmt->execute([$sizeID, $name]);
+    }
 }
 
 ?>
