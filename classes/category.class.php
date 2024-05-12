cf0294eb5f57b3d0135ae761bde7451e147d99d1
dimochii <diana.mm.nunes.28@gmail.com>
2024-05-12 17:34:11 +0100

    add category get and add functions

--- classes/category.class.php
+++ classes/category.class.php
@@ -10,6 +10,29 @@
       $this->categoryID = $categoryID;
       $this->name = $name;
     }
+
+    static function getCategory(PDO $db, int $categoryID) {
+      $preparedStmt = $db->prepare('SELECT * FROM Category WHERE categoryID = ?');
+      $preparedStmt->execute(array($categoryID));
+      $category = $preparedStmt->fetch();
+  
+      if (!$category) {
+          throw new Exception("Category not found with ID: $categoryID");
+          return null;
+      }
+  
+      return new Category(
+          $category['categoryID'],
+          $category['name']
+      );
+    }
+
+    static function addCategory(PDO $db, int $categoryID, string $name){
+      $preparedStmt = $db->prepare("INSERT INTO Category (categoryID, name) VALUES(?, ?)");
+      $preparedStmt -> execute([$categoryID, $name]);
+    }
+
+
 	}
 
 ?>
