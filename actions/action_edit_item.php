12bd4ff1166b24d6c72cfdcca215ae701ee67a1c
dimochii <diana.mm.nunes.28@gmail.com>
2024-05-15 20:58:12 +0100

    trying to make the itemId work

--- actions/action_edit_item.php
+++ actions/action_edit_item.php
@@ -18,10 +18,10 @@
     $newConditionName = $_POST['newConditionName'];
     $newSizeName = $_POST['newSizeName'];
     $newPrice = $_POST['newPrice'];
-    $newBrand = $_POST['itemID'];
-    $newModel = $_POST['itemID'];
-    $newDescription = $_POST['itemID'];
-    $newImageID = $_POST['itemID']; // nao sei como é que isto funciona Costinha
+    $newBrand = $_POST['newBrand'];
+    $newModel = $_POST['newModel'];
+    $newDescription = $_POST['newDescription'];
+    //$newImageID = $_POST['itemID']; // nao sei como é que isto funciona Costinha
 
     $item = Item::getItem($db, $itemID);
     $categoryID = Category::getCategoryByName($db, $categoryName);
@@ -37,9 +37,11 @@
         $editBrand = Item::editBrand($db, $item, $newBrand);
         $editModel = Item::editModel($db, $item, $newModel);
         $editDescription = Item::editDescription($db, $item, $newDescription);
-        $editImage = Item::editImage($db, $item, $newImageID);
+        //$editImage = Item::editImage($db, $item, $newImageID);
         if($editName || $editCategory || $editCondition || $editSize || $editPrice
-        || $editBrand || $editModel || $editDescription || $editImage){
+        || $editBrand || $editModel || $editDescription 
+        //|| $editImage
+        ){
         $session->addMessage('success', 'Edit Item successful!');
         }
         header('Location: /../pages/editItem.php');
