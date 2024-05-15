883c88592f301ca829d53cb33ee4061d140d4809
beatrizbernardoo <beatrizxalexandra@gmail.com>
2024-05-15 18:13:47 +0100

    editItem

---
+++ pages/editItem.php
@@ -0,0 +1,16 @@
+<?php 
+    require_once(__DIR__ . '/../database/connectdb.php');
+    require_once(__DIR__ . '/../classes/session.class.php');
+    require_once(__DIR__ . '/../classes/user.class.php');
+    require_once(__DIR__ . '/../templates/editItem.php');
+    require_once(__DIR__ . '/../templates/searchForm.php');
+    require_once(__DIR__ . '/../templates/topo.php');
+
+    $session = new Session();
+    $db = getDatabaseConnection();
+    $userID = $session->getID();
+    $user = User::getUser($db, $userID);
+    topo($db, $user);
+    displayEditItem();
+    editItemForm();
+?>
